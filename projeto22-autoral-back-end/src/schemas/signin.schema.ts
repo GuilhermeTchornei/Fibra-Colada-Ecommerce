78898e22ba065f2436e3ef261c899dd39f3a3b4c
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-07-03 17:58:48 -0300

    merge

--- projeto22-autoral-back-end/src/schemas/signin.schema.ts
+++
@@ -1,6 +0,0 @@
-import * as Joi from "joi";
-
-export const signinSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-});
--- projeto22-autoral-back-end/src/schemas/signup.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from "joi";
-
-export const signupSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
-    name: Joi.string().required(),
-    phone: Joi.string().length(11).required()
-});
